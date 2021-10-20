52d68e1e74b5c827ebeeb1715c92838a0981932f
Othman Nouaoury <noouaoury_othman@emsi-edu.ma>
2021-10-20 21:47:20 +0100

    projet

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebService_JaxWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SR1.BanqueServiceClient stub = new SR1.BanqueServiceClient();
+            Console.WriteLine(stub.ConversionEuroToDH(300));
+            SR1.compte cp = stub.getCompte(5);
+            Console.WriteLine(cp.code);
+            Console.WriteLine(cp.montant);
+            SR1.compte[] cptes = stub.compteList();
+            foreach(var c in cptes)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine(cp.code);
+                Console.WriteLine(cp.montant);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
